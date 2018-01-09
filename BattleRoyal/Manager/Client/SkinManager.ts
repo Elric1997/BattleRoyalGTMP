6f5d8491d2960e3de9810aaa812a63aa790147fe
thilo <thilobarth@hotmail.de>
2018-01-09 19:37:59 +0100

    Add Skin Array

--- BattleRoyal/Manager/Client/SkinManager.ts
+++ BattleRoyal/Manager/Client/SkinManager.ts
@@ -1,3 +1,14 @@
 ﻿var selectingSkin = false;
 var currentSkinIndex = 0;
 
+var skins = [
+    1581098148, // CopMale
+    368603149, // CopFemale
+    -459818001,
+    712602007,
+    1161072059,
+    1309468115,
+    349505262
+
+];
+
