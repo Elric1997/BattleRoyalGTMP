ebb8f84968ffc65176be4b08ebbe3e0402396921
Vumski <TMDuttke@web.de>
2018-01-10 18:37:45 +0100

    Add -Spawn Vector

--- BattleRoyal/Manager/SpawnManager.cs
+++ BattleRoyal/Manager/SpawnManager.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared.Math;
 
 namespace BattleRoyal.Manager
 {
-    class SpawnManager
+    public class SpawnManager : Script
     {
-        //Test 
+        public SpawnManager()
+        {
+            API.onPlayerFinishedDownload += OnPlayerFinishedDownloadHandler;
+
+        }
+        private void OnPlayerFinishedDownloadHandler(Client Player)
+        {
+            Vector3 SpawnPos = new Vector3(0, 0, 0); // Hier die SpawnPos
+        }
     }
 }
