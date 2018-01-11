6879a50ed5c9864c28a8a9536be3952385d7abe4
thilo <thilobarth@hotmail.de>
2018-01-11 17:57:06 +0100

    EventHandler Addet

---
+++ BattleRoyal/Manager/Events/onClientEventTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared.Math;
+
+namespace BattleRoyal.Manager.Events
+{
+    public class onClientEventTrigger : Script
+    {
+        public onClientEventTrigger()
+        {
+            API.onClientEventTrigger += OnClientEvent;
+        }
+        private void OnClientEvent(Client player, string eventName, params object[] arguments)
+        {
+            if(eventName == "Test")
+            {
+                API.consoleOutput("Test Event Triggert!");
+            }
+        }
+    }
+}
