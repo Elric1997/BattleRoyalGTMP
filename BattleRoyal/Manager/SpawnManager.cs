595bba2403ba2fab575f139c4f5f919cd69cf678
Vumski <TMDuttke@web.de>
2018-01-12 20:40:20 +0100

    Spawnmanagerdings weiter blub hi thilo this is very important <3

--- BattleRoyal/Manager/SpawnManager.cs
+++ BattleRoyal/Manager/SpawnManager.cs
@@ -8,19 +8,30 @@ using GrandTheftMultiplayer.Shared;
 using GrandTheftMultiplayer.Server.Elements;
 using GrandTheftMultiplayer.Server.Managers;
 using GrandTheftMultiplayer.Shared.Math;
+//https://wiki.gt-mp.net/index.php?title=Client
+// (0, 0, 0) X Y Z
+//
 
 namespace BattleRoyal.Manager
 {
     public class SpawnManager : Script
     {
+        public int conection = -3;
+
         public SpawnManager()
         {
             API.onPlayerFinishedDownload += OnPlayerFinishedDownloadHandler;
-
         }
         private void OnPlayerFinishedDownloadHandler(Client Player)
         {
+            conection--;
+
+            Vector3 RotationsPositon = new Vector3(0, 0, 0);
             Vector3 SpawnPos = new Vector3(0, 0, 0); // Hier die SpawnPos
+
+            API.setEntityPosition(Player, SpawnPos);
+            API.setEntityRotation(Player, RotationsPositon);
+            API.setEntityDimension(Player, conection);
         }
     }
 }
